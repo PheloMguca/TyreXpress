d36680651e4ce78e82ef8e44ef341114ef40ddd2
PheloMguca <230707726@mycput.ac.za>
2026-07-28 17:00:04 +0200

    OrderService & Controller pojo OrderLineService & Controller pojo

---
+++ src/test/java/za/ac/cput/repository/OrderLineRepositoryTest.java
@@ -0,0 +1,40 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderLineRepositoryTest {
+    private IOrderLineRepository repository;
+
+    @BeforeEach
+    void setup() {
+        OrderLineRepository.getRepository();
+    }
+
+    @Test
+    void getRepository() {
+
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
