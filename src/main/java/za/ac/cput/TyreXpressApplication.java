34664ab9fced1caab424da2253107769ac246525
Siyalezo <221789812@mycput.ac.za>
2026-08-19 16:25:14 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/za/ac/cput/TyreXpressApplication.java
+++ src/main/java/za/ac/cput/TyreXpressApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TyreXpressApplication {
     public static void main(String[] args) {
         SpringApplication.run(TyreXpressApplication.class, args);
+
     }
 }
